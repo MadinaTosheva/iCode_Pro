9317dc4be37f7f10a810c32431b6e1386737b06a
madinabonu.tosheva <madinatoseva4@gmail.com>
2025-08-14 11:51:31 +0500

    First day assignements are done!!

---
+++ HW_1/Task_1.py
@@ -0,0 +1,35 @@
+#    Дано:
+#      a = 5
+#      v = "abc"
+#      c = a + v
+#    Чему равен print(с) ??
+
+a = 5
+v = "abc"
+c = a + v
+print(c) # В данном случае получаем ошибку "TypeError", где говорится, что нельзя сложить 'int' и 'str'
+
+# Но есть другой вариант их сложение - type casting
+
+# Решение 1
+integer1 = str(5)
+string1 = "abc"
+sum1 = integer1 + string1
+print(sum1)                                        ## sum1 = 5abc
+
+# Решение 2
+integer2 = 5
+string2 = "abc"
+sum2 = str(integer2) + string2
+print(sum2)                                       ## sum2 = 5abc
+
+
+
+
+
+
+
+
+
+
+
